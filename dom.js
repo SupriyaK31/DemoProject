0777076c9f3ec4875298e6c1fe26f92841aa1e20
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2023-01-11 00:11:27 +0530

    final

--- dom.js
+++ dom.js
@@ -5,10 +5,18 @@ for(var i=0;i<li.length;i++)
 li[i].style.fontWeight = 'bold';
 }
 
-var li=document.getElementsByClassName('list-group-item');
+//var li=document.getElementsByClassName('list-group-item');
 
-for(var i=0;i<li.length;i++)
+//for(var i=0;i<li.length;i++)
+//{
+//li[i].style.backgroundColor = 'yellow';
+//}
+
+var odd= document.querySelectorAll('li:nth-child(odd)');
+var even= document.querySelectorAll('li:nth-child(even)');
+
+even[0].style.color='green';
+for(var i=0;i<odd.length;i++)
 {
-li[i].style.backgroundColor = 'yellow';
+    odd[i].style.backgroundColor='green';
 }
-

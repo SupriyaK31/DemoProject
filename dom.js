3d7b3a2648a483e5061894da3886f9dbe9e0d10d
SupriyaK31 <supriyakittur31@gmail.com>
2023-01-10 23:34:32 +0530

    Initial commit of all files to the repository

---
+++ dom.js
@@ -0,0 +1,14 @@
+var li=document.getElementsByTagName('li');
+
+for(var i=0;i<li.length;i++)
+{
+li[i].style.fontWeight = 'bold';
+}
+
+var li=document.getElementsByClassName('list-group-item');
+
+for(var i=0;i<li.length;i++)
+{
+li[i].style.backgroundColor = 'yellow';
+}
+
